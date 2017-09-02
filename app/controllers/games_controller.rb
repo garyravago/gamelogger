4eaf6f01155346f0a11b2321fb1795e9ab7e5832
Gary Ravago <ravagog@gmail.com>
2017-09-02 14:09:32 -0700

    Add Game new, create, show, and edit actions and views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,22 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    redirect_to root_url
+  end
+
+  def game_params
+    params.require(:game).permit(:title, :system, :genre, :start_date, :status)
   end
 end
